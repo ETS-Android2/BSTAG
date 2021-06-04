82cf3e3f8eaaa30c3d20059c58eb98251bae642b
nohhow <xksrma97@gmail.com>
2021-06-04 14:37:20 +0900

    MyApp 기능 구현, 앱패키지바로가기

--- app/src/main/java/com/example/nfcwriter/MyApp.java
+++ app/src/main/java/com/example/nfcwriter/MyApp.java
@@ -3,14 +3,22 @@ package com.example.nfcwriter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,14 +30,25 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyApp extends AppCompatActivity {
 
+    //myApp List
     private List<AppList> installedApps;
     private AppAdapter installedAppAdapter;
     ListView userInstalledApps;
+
+
+    //태그
+    boolean mWriteMode = false;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mNfcPendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +64,31 @@ public class MyApp extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
 
-                String[] colors = {" Open App", " App Info"};
+                String[] colors = {" NFC 태그에 기록하기", " 앱 정보"};
                 AlertDialog.Builder builder = new AlertDialog.Builder(MyApp.this);
                 builder.setTitle("Choose Action")
                         .setItems(colors, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 // The 'which' argument contains the index position of the selected item
                                 if (which==0){
-                                    Intent intent = getPackageManager().getLaunchIntentForPackage(installedApps.get(i).packages);
-                                    if(intent != null){
-                                        startActivity(intent);
-                                    }
-                                    else {
-                                        Toast.makeText(MyApp.this, installedApps.get(i).packages + " Error, Please Try Again...", Toast.LENGTH_SHORT).show();
-                                    }
+                                    TextView setNum = (TextView)findViewById(R.id.selectNum);
+                                    setNum.setText(installedApps.get(i).packages);
+
+                                    mNfcAdapter = NfcAdapter.getDefaultAdapter(MyApp.this);
+                                    mNfcPendingIntent = PendingIntent.getActivity(MyApp.this, 0,
+                                            new Intent(MyApp.this, MyApp.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+                                    enableTagWriteMode();
+
+                                    new AlertDialog.Builder(MyApp.this).setTitle("Touch tag to write")
+                                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                                @Override
+                                                public void onCancel(DialogInterface dialog) {
+                                                    disableTagWriteMode();
+                                                }
+
+                                            }).create().show();
+
                                 }
                                 if (which==1){
                                     Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
@@ -175,6 +205,80 @@ public class MyApp extends AppCompatActivity {
             return packages;
         }
 
+    }
+
+    //태그 관련 메소드
+
+    //tagwrite 모드 활성화
+    private void enableTagWriteMode() {
+        mWriteMode = true;
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter[] mWriteTagFilters = new IntentFilter[] { tagDetected };
+        mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mWriteTagFilters, null);
+    }
+    //tagwrite 모드 비활성화
+    private void disableTagWriteMode() {
+        mWriteMode = false;
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Tag writing mode
+        if (mWriteMode && NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            TextView setNum = (TextView)findViewById(R.id.selectNum);
+            String packageName = setNum.getText().toString();
+
+            NdefRecord record = NdefRecord.createApplicationRecord(packageName);
+            NdefMessage message = new NdefMessage(new NdefRecord[] { record });
+            if (writeTag(message, detectedTag)) {
+                Toast.makeText(this, "기록 성공! : 앱 바로가기가 기록되었습니다.", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    /*
+     * Writes an NdefMessage to a NFC tag
+     */
+    public boolean writeTag(NdefMessage message, Tag tag) {
+        int size = message.toByteArray().length;
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag not writable",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                if (ndef.getMaxSize() < size) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: tag too small",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                ndef.writeNdefMessage(message);
+                return true;
+            } else {
+                NdefFormatable format = NdefFormatable.get(tag);
+                if (format != null) {
+                    try {
+                        format.connect();
+                        format.format(message);
+                        return true;
+                    } catch (IOException e) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 
