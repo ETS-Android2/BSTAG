1441f649d5d7222efd2ebe8275b3ab1a3ace852d
nohhow <xksrma97@gmail.com>
2021-06-18 00:47:34 +0900

    totalk 바로가기까지 완료

--- app/src/main/java/com/example/nfcwriter/MainActivity.java
+++ app/src/main/java/com/example/nfcwriter/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.nfcwriter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,Etiquette.class);
                 startActivity(intent);
+            }
+        });
+
+        ((ImageButton)findViewById(R.id.btn8)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.allthatmobilenfc");
+                startActivity( launchIntent );
             }
         });
 
